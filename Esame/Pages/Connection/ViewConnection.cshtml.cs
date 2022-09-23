d54a8c852773a8cfa5ec3a37041b094063f2ef03
Lorenzo Fabbro <20033848@studenti.uniupo.it>
2022-09-23 13:39:28 +0200

    altre info sulle query

--- Esame/Pages/Connection/ViewConnection.cshtml.cs
+++ Esame/Pages/Connection/ViewConnection.cshtml.cs
@@ -39,7 +39,7 @@ namespace Esame.Pages.Connection
                 QueryTest();
             }
         }
-        
+
 
         public void QueryTest()
         {
@@ -48,8 +48,38 @@ namespace Esame.Pages.Connection
                 SqliteConnection o = JsonConvert.DeserializeObject<SqliteConnection>(SqliteTable[i].Connection);
                 IList<String> tabelle = new List<String>();
                 o.Open();
-                
-                
+
+
+                // INFORMAZIONI SUL TIPO DI COLONNE
+                /*
+                var cmd = new SqliteCommand("PRAGMA table_info("+"SqliteOpenConnections"+")", o);
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())//loop through the various columns and their info
+                {
+                    Console.WriteLine("INFO: " + dr.GetString(0)+" "+ dr.GetString(1)+" "+ dr.GetString(2)+ " " + dr.GetString(3));
+                }
+
+                dr.Close();
+                */
+
+                /*INFORMAZIONI SULLA CREATE DI OGNI TABELLA
+                 
+                var sql= "select* from sqlite_master where type = 'table' and name = 'SqliteOpenConnections'";
+                //var cmd = new SqliteCommand("PRAGMA table_info("+"SqliteOpenConnections"+") as l WHERE l.pk=1", o);
+                var cmd= new SqliteCommand(sql,o);
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    Console.WriteLine("INFO TABELLA: " + dr.GetString(0) + " " + dr.GetString(4));
+
+                }
+
+                */
+
+
+                /* INFO SUL NOME DELLE TABELLE 
+
                 var sql = "SELECT name FROM sqlite_master WHERE type='table'";
                 
                 using var cmd = new SqliteCommand(sql, o);
@@ -61,6 +91,20 @@ namespace Esame.Pages.Connection
                     tabelle.Add(rdr.GetString(0));
                 }
 
+
+                
+                /*
+                var sql = $"use{o.Database} SELECT * FROM {o.GetSchema()}";
+
+                using var cmd = new SqliteCommand(sql, o);
+                using SqliteDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+
+                    Console.WriteLine("Tabella: " + rdr.GetString(0));
+                    tabelle.Add(rdr.GetString(0));
+                }
+                */
                 /*
                 var sql1 = $"SELECT * FROM {tabelle[3]}";
 
@@ -73,7 +117,7 @@ namespace Esame.Pages.Connection
                     
                 }
                 */
-                
+
 
 
 
@@ -81,6 +125,6 @@ namespace Esame.Pages.Connection
 
             }
         }
-        
+
     }
 }
