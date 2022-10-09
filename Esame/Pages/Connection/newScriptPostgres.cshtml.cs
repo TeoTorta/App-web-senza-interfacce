e33b6e814c5a82af03bc59e5cf1883c8dbfa7c28
Matteo Torta <20034002@studenti.uniupo.it>
2022-10-09 18:19:23 +0200

    inizio postgres script

--- Esame/Pages/Connection/newScriptPostgres.cshtml.cs
+++ Esame/Pages/Connection/newScriptPostgres.cshtml.cs
@@ -1,15 +1,62 @@
+using Esame.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Npgsql;
 
 namespace Esame.Pages.Connection
 {
-    public class newScriptModel : PageModel
+    public class newScriptPostgresModel : PageModel
     {
-        public void OnGet()
+        private readonly ConnectionContext _context;
+
+        [BindProperty]
+        public Test prova { get; set; } = default;
+        public String stringa { get; set; }
+
+        public newScriptPostgresModel(ConnectionContext context)
         {
+            _context = context;
+
         }
 
+        [BindProperty]
+        public static PostgresOpenConnection Input { get; set; }
 
-        //public IActionResult OnPost() { }
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Input = await _context.PostgresOpenConnections.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Input == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Console.WriteLine(Input.Database);
+            string connectionString = $"Host={Input.Host}; Database={Input.Database}; User ID={Input.UserId}; Password={Input.Password};";
+            NpgsqlConnection o = new NpgsqlConnection(connectionString);
+
+            o.Open();
+
+            Console.WriteLine(o.State);
+
+            var cmd = new NpgsqlCommand(prova.query, o);
+            var dr = cmd.ExecuteReader();
+
+
+            while (dr.Read())//loop through the various columns and their info
+            {
+                Console.WriteLine(dr.GetString(0));
+            }
+            return Page();
+        }
     }
 }
